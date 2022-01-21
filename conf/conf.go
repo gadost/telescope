1e3a0a85c34b8197a2a4c5e599cd6b53a9cf4f35
Max N <nikolaev.makc@gmail.com>
2022-01-21 11:42:58 +0200

    init

--- conf/conf.go
+++ conf/conf.go
@@ -11,41 +11,13 @@ import (
 )
 
 type Config struct {
-    Title   string
-    Owner   ownerInfo
-    DB      database `toml:"database"`
-    Servers map[string]server
-    Clients clients
-}
 
-type ownerInfo struct {
-    Name string
-    Org  string `toml:"organization"`
-    Bio  string
-    DOB  time.Time
-}
-
-type database struct {
-    Server  string
-    Ports   []int
-    ConnMax int `toml:"connection_max"`
-    Enabled bool
-}
-
-type server struct {
-    IP string
-    DC string
-}
-
-type clients struct {
-    Data  [][]interface{}
-    Hosts []string
 }
 
 var conf Config
 
 func ConfigExist() {
-    f := "~/.telescope/example.toml"
+    f := "~/.telescope/config.toml"
     if _, err := os.Stat(f); err != nil {
         log.Printf(errors.ConfNotFound, err)
     }
