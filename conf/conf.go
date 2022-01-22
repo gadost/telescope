ddfa16c75a86179637465c039038ab3853cb3f11
Max N <nikolaev.makc@gmail.com>
2022-01-22 22:32:02 +0200

    config

--- conf/conf.go
+++ conf/conf.go
@@ -1,42 +1,78 @@
 package conf
 
 import (
-    "log"
-    "os"
+	"flag"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 
-    "github.com/gadost/telescope/errors"
-
-    "github.com/BurntSushi/toml"
+	"github.com/BurntSushi/toml"
 )
 
-type Config struct {
-    Chains map[string]chain
+var configPath string
+var userHome = os.Getenv("HOME")
+var cfg Config
+var infra nodes
+var chains []string
+
+func init() {
+
+	flag.StringVar(&configPath, "confd", userHome+"/.telescope/conf.d", "path to configs dir")
+	flag.Parse()
+
 }
 
-type chain struct {
-    Name  string
-    Nodes map[string]node
+//Chain struct for chain.toml configs
+type Config struct {
+	Chain map[string]nodes
+}
+
+type nodes struct {
+	Node []node
 }
 
 type node struct {
-    Role                  string
-    Address               string
-    NetworkMonitorEnabled bool
+	Role                  string
+	Address               string
+	NetworkMonitorEnabled bool
 }
 
-var conf Config
-
-func ConfExist() {
-    f := "~/.telescope/config.toml"
-    if _, err := os.Stat(f); err != nil {
-        log.Printf(errors.ConfNotFound, err)
-    }
-
+//Check existence of confd folder
+func ConfLoad() (Config, []string) {
+	if _, err := os.Stat(configPath); !os.IsNotExist(err) {
+		files, err := ioutil.ReadDir(configPath)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		buildConf(files)
+	}
+	return cfg, chains
 }
 
-func ConfLoad(data string) {
-    _, err := toml.Decode(data, &conf)
-    if err != nil {
-        panic(errors.ConfInvalid)
-    }
+// buildConf load parsed config to struct
+func buildConf(files []fs.FileInfo) {
+
+	for _, f := range files {
+		if _, err := toml.DecodeFile(configPath+"/"+f.Name(), &infra); err != nil {
+			log.Fatal(err)
+		}
+		for _, s := range infra.Node {
+			fmt.Printf("%s (%s)\n", s.Address, s.Role)
+		}
+		//prevent panic on nil map
+		if cfg.Chain == nil {
+			cfg.Chain = make(map[string]nodes)
+		}
+		cfg.Chain[fileNameWithoutExtSliceNotation(f.Name())] = infra
+		chains = append(chains, fileNameWithoutExtSliceNotation(f.Name()))
+	}
+}
+
+// Remove extesion  for add to chains slice
+func fileNameWithoutExtSliceNotation(fileName string) string {
+	return fileName[:len(fileName)-len(filepath.Ext(fileName))]
 }
